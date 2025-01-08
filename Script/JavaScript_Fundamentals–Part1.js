a3fc2846e7413f50034506d7d875c7f048477a72
H82M19\Student <Student>
2025-01-08 10:26:45 +0700

    JavaScript Fundamentals – Part 1 Coding Challenge #2

--- Script/JavaScript_Fundamentals–Part1.js
+++ Script/JavaScript_Fundamentals–Part1.js
@@ -6,7 +6,7 @@ var BMIJohn1;
 let markHigherBMI = false;
 
 console.log("------ Data 1 ------")
-if(true) {
+if (true) {
     mass = 78;
     height = 1.69;
 
@@ -14,7 +14,7 @@ if(true) {
     console.log("Marks weights 78 kg and is 1.69 m tall => BMI: " + BMIMarks1);
 }
 
-if(true) {
+if (true) {
     mass = 92;
     height = 1.95;
 
@@ -22,16 +22,16 @@ if(true) {
     console.log("John weights 92 kg and is 1.95m tall => BMI: " + BMIJohn1);
 }
 
-if(BMIMarks1 > BMIJohn1) {
+if (BMIMarks1 > BMIJohn1) {
     console.log("Mark has a higher BMI than John.");
-}else {
+} else {
     console.log("John has a higher BMI than Mark.");
 }
 
 console.log("------ Data 2 ------")
 var BMIMarks2;
 var BMIJohn2;
-if(true) {
+if (true) {
     mass = 95;
     height = 1.88;
 
@@ -39,7 +39,7 @@ if(true) {
     console.log("Marks weights 78 kg and is 1.69 m tall => BMI: " + BMIMarks2);
 }
 
-if(true) {
+if (true) {
     mass = 85;
     height = 1.76;
 
@@ -47,8 +47,58 @@ if(true) {
     console.log("John weights 92 kg and is 1.95m tall => BMI: " + BMIJohn2);
 }
 
-if(BMIMarks2 > BMIJohn2) {
+if (BMIMarks2 > BMIJohn2) {
     console.log("Mark has a higher BMI than John.");
 } else {
     console.log("John has a higher BMI than Mark.");
+}
+
+
+console.log("--------------------------------------------------------------------------------------");
+console.log("Coding Challenge #2");
+
+console.log("------ Data 1 ------")
+if (true) {
+    mass = 78;
+    height = 1.69;
+
+    BMIMarks1 = mass / (height * height);
+    console.log("Marks weights 78 kg and is 1.69 m tall => BMI: " + BMIMarks1);
+}
+
+if (true) {
+    mass = 92;
+    height = 1.95;
+
+    BMIJohn1 = mass / (height * height);
+    console.log("John weights 92 kg and is 1.95m tall => BMI: " + BMIJohn1);
+}
+
+if (BMIMarks2 > BMIJohn2) {
+    console.log("Mark (" + BMIMarks2 + ")" + "  has a higher BMI than John (" + BMIJohn2 + ")");
+} else {
+    console.log("John (" + BMIJohn2 + ")" + " has a higher BMI than Mark (" + BMIMarks2 + ")");
+}
+
+console.log("------ Data 2 ------")
+if (true) {
+    mass = 95;
+    height = 1.88;
+
+    BMIMarks2 = mass / (height * height);
+    console.log("Marks weights 78 kg and is 1.69 m tall => BMI: " + BMIMarks2);
+}
+
+if (true) {
+    mass = 85;
+    height = 1.76;
+
+    BMIJohn2 = mass / (height * height);
+    console.log("John weights 92 kg and is 1.95m tall => BMI: " + BMIJohn2);
+}
+
+if (BMIMarks2 > BMIJohn2) {
+    console.log("Mark (" + BMIMarks2 + ")" + "  has a higher BMI than John (" + BMIJohn2 + ")");
+} else {
+    console.log("John (" + BMIJohn2 + ")" + " has a higher BMI than Mark (" + BMIMarks2 + ")");
 }
